dffc68cdbdcfc912ae98a69bd235c2970e96ff7e
EliteHacker228 <mchappycraft@yandex.ru>
2024-10-27 22:46:51 +0500

    style: remove redundant code and commentaries

--- YOLO/train/train.py
+++ YOLO/train/train.py
@@ -1,15 +1,5 @@
 from ultralytics import YOLO
 
-# Замените 'yolov11.pt' на путь к предобученной модели YOLOv11, если необходимо
 model = YOLO('../models/yolo11n.pt')
-
-# Запустите обучение
 model.train(data='../dataset/data.yaml', epochs=50, imgsz=640)
-
-# Оценка модели на валидационном наборе данных
-results = model.val()
-
-# Сохранение модели
 model.save('../result/ood_model_yolo.pt')
-
-# results = model.predict(source='path/to/test/images', save=True)
